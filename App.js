692c2a91a3df86749f4dd2c3dbb841fbc503a7e8
LakshTheNinja <87697222+LakshTheNinja@users.noreply.github.com>
2023-06-18 14:47:18 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactContact from './components/reactcontact';
+
+function App() {
+  return (
+   <>
+   <ReactContact/>
+   </>
+  );
+}  
+
+export default App;
